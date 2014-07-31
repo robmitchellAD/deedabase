1f514db484db48ec8d4bfb9df16f9be52b70e38e
Rob Mitchel <rob.mitchel@astonishdesign.com>
2014-07-31 15:12:12 -0500

    Intialize Deedabase repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Deedabase::Application.config.secret_key_base = '24231000530b02c3a6339b73f22ca251fe913c7e9e654403316d08f45f6d2adc76d9b4fe92b1bfce4eb11564e736a650c47b8511db3576055f15ff31a2e738c4'
